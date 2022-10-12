439be16503a63c8ec54621509adc91e3b2e312db
Julius Ikkala <julius.ikkala@tuni.fi>
2022-10-12 10:11:48 +0300

    Change default of envmap importance sampling

--- src/options.hh
+++ src/options.hh
@@ -390,7 +390,7 @@
         "Importance sample the environment map, if present. Has a minor " \
         "performance hit, and can make some rare scenes noisier, but " \
         "generally reduces noise significantly.", \
-        true \
+        false \
     )
 //==============================================================================
 // END OF OPTIONS
